5877c3d24dd18f574b09caa176740e50d37cda06
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-16 19:37:31 +0530

    Add files via upload

--- codes/fredat.py
+++ codes/fredat.py
@@ -1,21 +1,34 @@
-data = [10,20,36,92,95,40,50,56,60,70,92,88,80,70,72,70,36,40,36,40,92,40,50,50,56,60,70,60,60,88]
+import numpy as np
+import pandas as pd
+import openpyxl
+from openpyxl.styles import Alignment
+from collections import Counter
 
-total = len(data)
+df=pd.read_excel('table.xlsx','Sheet1',header=None)
+a = df.to_numpy()
+df=pd.DataFrame(a)
+newlst = []
+df.applymap(lambda x: newlst.append(x))
+newlst.sort()
 
-print("marks frequency\n")
-print("10  ","  1")
-print("20  ","  1")
-print("36  ","  3")
-print("40  ","  4")
-print("50  ","  3")
-print("56  ","  2")
-print("60  ","  4")
-print("70  ","  4")
-print("72  ","  1")
-print("80  ","  1")
-print("88  ","  2")
-print("92  ","  3")
-print("95  ","  1")
+cou=Counter(newlst)
+marks=list(cou.keys())
+freq=list(cou.values())
 
+total = len(newlst)
 
-print("\nTotal ",total)
+df1 = pd.DataFrame()
+df1['marks']=marks
+df1['Number of students (i.e.,the frequency)']=freq
+df1.to_excel('frequency.xlsx',index=False)
+
+wb=openpyxl.load_workbook("frequency.xlsx")
+ws=wb['Sheet1']
+ws['A15']='Total'
+ws['B15']=total
+ws.column_dimensions['B'].width = 25
+ws.row_dimensions[1].height = 30
+ws['B1'].alignment = Alignment(wrap_text=True)
+
+wb.save("frequency.xlsx")
+wb.close()

cf113cd9728a950338c845d003b14d76e8518f57
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-16 12:48:05 +0530

    Add files via upload

---
+++ codes/fredat.py
@@ -0,0 +1,21 @@
+data = [10,20,36,92,95,40,50,56,60,70,92,88,80,70,72,70,36,40,36,40,92,40,50,50,56,60,70,60,60,88]
+
+total = len(data)
+
+print("marks frequency\n")
+print("10  ","  1")
+print("20  ","  1")
+print("36  ","  3")
+print("40  ","  4")
+print("50  ","  3")
+print("56  ","  2")
+print("60  ","  4")
+print("70  ","  4")
+print("72  ","  1")
+print("80  ","  1")
+print("88  ","  2")
+print("92  ","  3")
+print("95  ","  1")
+
+
+print("\nTotal ",total)
